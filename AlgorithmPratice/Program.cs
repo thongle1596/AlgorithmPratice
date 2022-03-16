66ecf5e99eca6731fba9d43bb7eac6fd0d96d9d3
Thong Le <jasonle1596@gmail.com>
2022-03-16 23:51:40 +0700

    Upload source code

---
+++ AlgorithmPratice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static AlgorithmPratice.Sort.SortAlgorithm;
+
+namespace AlgorithmPratice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 12, 2, 8, 5, 1, 6, 4, 15 };
+            int[] b = { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int n = a.Length;
+            int m = b.Length;
+
+            Console.WriteLine("Before: " + "[{0}]", string.Join(", ", a));
+
+            InterchangeSort(a, m);
+            BubbleSort(a, n);
+            InsertionSort(a, n);
+            SelectionSort(a, n);
+
+            Console.WriteLine("After: " + "[{0}]", string.Join(", ", a));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlgorithmPratice/Sort/SortAlgorithm.cs
@@ -0,0 +1,76 @@
+﻿namespace AlgorithmPratice.Sort
+{
+    class SortAlgorithm
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void InterchangeSort(int[] a, int n)
+        {
+            for (int i = 0; i < n-1; i++)
+            {
+                for( int j = i + 1; j < n ; j++)
+                {
+                    if(a[i] > a[j])
+                    {
+                        Swap(ref a[i], ref a[j]);
+                    }
+                }
+            }
+        }
+        
+        public static void BubbleSort(int[] a, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    if (a[j] < a[j - 1])
+                    {
+                        Swap(ref a[j], ref a[j - 1]);
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] a, int n)
+        {
+            int pos, x;
+            for(int i = 1; i < n; i++)
+            {
+                pos = i;
+                x = a[i];
+                while(pos > 0 && x < a[pos - 1])
+                {
+                    a[pos] = a[pos - 1];
+                    pos--;
+                }
+                a[pos] = x;
+            }
+        }
+
+        public static void SelectionSort(int[] a, int n)
+        {
+            int min;
+            for(int i = 0; i < n - 1; i++)
+            {
+                min = i;
+                for(int j = i + 1; j < n; j++)
+                {
+                    if (a[j] < a[min])
+                    {
+                        min = j;
+                    }
+                }
+                if(min != i)
+                {
+                    Swap(ref a[min], ref a[i]);
+                }
+            }
+        }
+    }
+}
